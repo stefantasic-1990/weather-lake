6bc6774de75f88c7ee820a0c4129b08519169360
Stefan Tasic <stefantasic.2790@gmail.com>
2025-10-08 09:03:19 -0400

    add and configure hive metastore and minio service

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,3 +1,7 @@
 -- Create Airflow user and database
 CREATE USER airflow WITH PASSWORD 'airflow';
 CREATE DATABASE airflow OWNER airflow;
+
+-- Create Hive user and metastore database
+CREATE USER hive WITH PASSWORD 'hivepass';
+CREATE DATABASE metastore OWNER hive;
