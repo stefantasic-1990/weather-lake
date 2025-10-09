6aeba5c2a5f5dadc3fcba597e2535c35f3d767cb
Stefan Tasic <stefantasic.2790@gmail.com>
2025-10-08 20:18:56 -0400

    working download data task

--- airflow/dags/weather-lake-ingestion.py
+++ airflow/dags/weather-lake-ingestion.py
@@ -1,5 +1,18 @@
-from airflow.decorators import dag, task
+import requests
+import hashlib
+from airflow.sdk import dag, task
 from datetime import datetime
+
+openmeteo_api_endpoint=(
+    "https://api.open-meteo.com/v1/forecast?"
+    "latitude=43.7064&longitude=-79.3986&"
+    "hourly=temperature_2m,relative_humidity_2m,precipitation,"
+    "wind_speed_10m,wind_direction_10m,pressure_msl&format=csv"
+)
+temp_file_dir="/opt/airflow/temp/"
+temp_file_name="data.csv"
+temp_file_path=temp_file_dir+temp_file_name
+chunk_size=8192
 
 @dag(
     start_date=datetime(2025, 1, 1),
@@ -10,7 +23,21 @@ def weather_lake_ingestion():
 
     @task
     def download_weather_data():
-        return
+        hasher=hashlib.sha256()
+
+        with requests.get(openmeteo_api_endpoint, stream=True, timeout=60) as req:
+            req.raise_for_status()
+
+            with open(temp_file_path, "wb") as temp_file:
+                for chunk in req.iter_content(chunk_size=chunk_size):
+                    if not chunk:
+                        continue
+                    hasher.update(chunk)
+                    temp_file.write(chunk)
+
+        temp_file_digest = hasher.hexdigest()
+        
+        return str(temp_file_path), temp_file_digest
 
     download_weather_data_task=download_weather_data()
 
